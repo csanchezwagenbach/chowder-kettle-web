1edd6b20e4af52702d84720eb51a3fa037fa2499
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-03-07 16:23:03 -0800

    reconfigure home page, ready to begin implementing user authentication

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import Head from 'next/head'
 import { Cedarville_Cursive } from 'next/font/google';
 import homeStyles from '../styles/Home.module.css';
-import ClamDigger from '../components/ClamDigger';
-import { firestore } from '../lib/firebase';
 
 const cursive = Cedarville_Cursive({ weight: ['400'], subsets: ['latin']});
-const userQuery = firestore.collection('users');
 
-const user = (await userQuery.get()).docs
 
-export default async function Home(user) {
+export default function Home() {
 
  
   return (
@@ -26,7 +22,7 @@ export default async function Home(user) {
         
         <h1 className={cursive.className}>You should have been with us that day . . .</h1>
 
-        <ClamDigger user={user} />
+
         
       </main>
     </>
