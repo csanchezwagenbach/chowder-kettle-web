0e0095f08edc0ab62d7658076cbebe6a685dc062
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-03-08 12:37:35 -0800

    debugging user authentication

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,92 @@
-export default function Login(props) {
+import { auth, firestore } from '../lib/firebase';
+import { useState, useRef, useEffect } from 'react';
+
+function SignIn() {
+    const [recaptcha, setRecaptcha] = useState(null);
+    const element = useRef(null);
+
+    useEffect(() => {
+        if (!recaptcha) {
+
+            const verifier = auth.default.RecaptchaVerifier(element.current, {
+                size: 'invisible',
+            })
+
+            verifier.verify().then(() => setRecaptcha(verifier));
+
+        }
+    });
+
     return (
         <>
-        <p>Here'll sound the barbaric yawp.</p>
+            {recaptcha && <PhoneNumberVerification recaptcha={recaptcha} />}
+            <div ref={element}></div>
+        </>
+    );
+}
+
+function PhoneNumberVerification({ recaptcha }) {
+    const [digits, setDigits] = useState('');
+    const [invited, setInvited] = useState(false);
+    const [confirmationResult, setConfirmationResult] = useState(null);
+    const [code, setCode] = useState('');
+
+    const phoneNumber = `+{digits}`;
+
+    // Verify Invite
+    useEffect(() => {
+        if (phoneNumber.length === 12) {
+            const ref = firestore.collection('invites').doc(phoneNumber);
+            ref.get().then(({ exists }) => { setInvited(exists) });
+        } else {
+            setInvited(false);
+        }
+    }, [phoneNumber]);
+
+    // Sign In
+    const signInWithPhoneNumber = async () => {
+        setConfirmationResult(await auth.signInWithPhoneNumber(phoneNumber, recaptcha));
+    };
+
+    // Verify SMS code
+    const verifyCode = async () => {
+        const result = await confirmationResult.confirm(code);
+        console.log(result.user);
+    };
+
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <fieldset>
+                <label>10 digit phone number</label>
+                <br />
+                <input value={digits} onChange={(e) => setDigits(e.target.value)} />
+
+                <button onClick={signInWithPhoneNumber}>
+                    Sign In
+                </button>
+            </fieldset>
+
+            {confirmationResult && (
+                <fieldset>
+                    <label>erify code</label>
+                    <br />
+                    <input value={code} onChange={(e) => setCode(e.target.value)} />
+
+                    <button onClick={verifyCode}>Verify Code</button>
+                </fieldset>
+            )}
+        </div>
+    );
+}
+
+
+
+export default function Login(props) {
+
+    return (
+        <>
+            <SignIn />
         </>
     )
 }
